f7fe73135032497a338f010d0cb4363a9e99627d
Freddy Ouma <oumafreddy@users.noreply.github.com>
2025-09-27 09:29:12 +0300

    Bugs and Fixes 27092025

--- config/settings/production.py
+++ config/settings/production.py
@@ -158,6 +158,7 @@ REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
 ]
 REST_FRAMEWORK['DEFAULT_AUTHENTICATION_CLASSES'] = [
     'rest_framework_simplejwt.authentication.JWTAuthentication',
+    'rest_framework.authentication.SessionAuthentication',
 ]
 
 # Performance settings
